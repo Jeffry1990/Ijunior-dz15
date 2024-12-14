7b9e9392c2af78e336d5e2e8dd2fea49bd781efc
Jeffry1990 <warlock_artur@mail.ru>
2024-12-14 08:37:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber;
+            int firstNumber = 10;
+            int secondNumber = 25;
+            int minNumberFound = 50;
+            int maxNumberFound = 150;
+
+            Console.Write($"Введите число от {firstNumber} до {secondNumber}: ");
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (inputNumber >= firstNumber && inputNumber <= secondNumber)
+            {
+                for (int i = minNumberFound; i <= maxNumberFound; i += inputNumber)
+                Console.WriteLine(i);
+                Console.WriteLine($"Числа от {minNumberFound} до {maxNumberFound} которые крытны {inputNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный диапазон чисел");
+            }
+        }
+    }
+}
